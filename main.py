e89b33d10e10090a46e7a6ea25297d924628ad90
Abu Darda <abu.darda@brainstation-23.com>
2024-04-26 11:38:22 +0600

    created main

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 print("hello")
+
+operation = input().split()
+
+ans = 0
+
+if operation[1] == '+': ans = add(operation[0], operation[2])
+if operation[1] == '-': ans = substract(operation[0], operation[2])
+if operation[1] == '*': ans = multiply(operation[0], operation[2])
+if operation[1] == '/': ans = divide(operation[0], operation[2])
+
