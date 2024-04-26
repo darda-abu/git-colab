9c236bc25192e533a23164fe2d668a0ec6d89f53
Md shehab Hossain <shehab.hossain@brainstation-23.com>
2024-04-26 12:42:16 +0600

    Addition function added.

--- main.py
+++ main.py
@@ -1,20 +1,44 @@
-def divide(a, b):
-    try:
-        result = a / b
-        return result
-    except ZeroDivisionError:
-        print("Error: Division by zero is not allowed")
-        return None
-def Addition(num1,num2):
-    return num1 + num2 
+class Calculator:
+    def __init__(self, num1, num2, symbol) -> None:
+        self.num1 = num1
+        self.num2 = num2 
+        self.symbol = symbol
+    
+    def substraction(self, num1, num2) -> int:
+        return self.num1 - self.num2 
+    
+    def addition(num1,num2):
+        return self.num1 + self.num2 
 
+    def multiplication(num1,num2):
+        return self.num1 * self.num2
 
-operation = input().split()
+    def division(self, num1, num2):
+        try:
+            result = self.num1 / self.num2
+            return result
+        except ZeroDivisionError:
+            print("Error: Division by zero is not allowed")
+            return None
 
-ans = 0
+    if __name__ == "__main__":
+        num1 = int(input())
+        num2 = int(input())
+        symbol = input()
+        calc_obj = Calculator(num1, num2, symbol)
 
-if operation[1] == '+': ans = add(operation[0], operation[2])
-if operation[1] == '-': ans = substract(operation[0], operation[2])
-if operation[1] == '*': ans = multiply(operation[0], operation[2])
-if operation[1] == '/': ans = divide(operation[0], operation[2])
+        if symbol == '-':
+            print("Substraction: ", calc_obj.substraction(num1, num2)) 
+        elif symbol == '/':
+            print("Division: ", calc_obj.division(num1, num2))
+            
+        elif symbol == '+':
+            print("Addition: ", calc_obj.addition(num1,num2))
+            
+        elif symbol == '*':
+            print("Multiplication: ", calc_obj.multiplication(num1,num2))
+            
+        else:
+            print("Invalild input, please press correct symbol for calculation")
+
 
